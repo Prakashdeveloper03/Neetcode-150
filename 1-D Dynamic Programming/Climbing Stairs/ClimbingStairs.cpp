ae3c7b4bad7f5d7fca2bb4583aca6b1933c88e87
Siva Prakash <prakashdeveloper@outlook.in>
2024-09-11 11:19:16 +0530

    Formatted Climbing Stairs folder

--- 1-D Dynamic Programming/Climbing Stairs/ClimbingStairs.cpp
+++ 1-D Dynamic Programming/Climbing Stairs/ClimbingStairs.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 using namespace std;
 class Solution {
-public:
-    int climbStairs(int n) {
-        if(n < 0) return 0;
-        if(n == 0) return 1;
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        int n1 = 1, n2 = 2, current;
-        for(int i = 2; i < n; i++) {
-            current = n1 + n2;
-            n1 = n2;
-            n2 = current;
-        }
-        return current;
+ public:
+  int climbStairs(int n) {
+    if (n < 0) return 0;
+    if (n == 0) return 1;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    int n1 = 1, n2 = 2, current;
+    for (int i = 2; i < n; i++) {
+      current = n1 + n2;
+      n1 = n2;
+      n2 = current;
     }
+    return current;
+  }
 };
-
 
 int main() {
   Solution obj;
